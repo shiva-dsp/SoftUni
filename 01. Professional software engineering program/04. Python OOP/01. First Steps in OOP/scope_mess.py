40228db2b5e86d06cd16861777c2defbd308f2fd
Emil Enchev <emoen4ev@gmail.com>
2022-07-01 16:46:30 +0300

    Added source code.

--- 01. Professional software engineering program/04. Python OOP/01. First Steps in OOP/scope_mess.py
+++ 01. Professional software engineering program/04. Python OOP/01. First Steps in OOP/scope_mess.py
@@ -56,3 +56,14 @@ inner: nonlocal
 outer: nonlocal
 global: changed!
 '''
+
+
+# def get_print_list_func(separator_char):
+#     def internal_print(ll):
+#         print(separator_char.join(str(x) for x in ll))
+#
+#     return internal_print
+#
+# print_list = get_print_list_func('; ')
+#
+# print_list([1, 2, 3])
