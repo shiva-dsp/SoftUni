d699f39f6286232f2731e29198c3a4e86a8e27cd
Emil Enchev <emoen4ev@gmail.com>
2022-07-01 13:11:59 +0300

    Added get_line function in rhombus_of_stars.py.

--- 01. Professional software engineering program/04. Python OOP/01. First Steps in OOP/rhombus_of_stars.py
+++ 01. Professional software engineering program/04. Python OOP/01. First Steps in OOP/rhombus_of_stars.py
@@ -19,16 +19,17 @@ n = 4
 '''
 
 
+def get_line(i, n):
+    spaces_count = n - 1 - i
+    stars_count = i + 1
+    return ' ' * spaces_count + '* ' * stars_count
+
+
 def print_rhombus(n):
     for i in range(n):
-        spaces_count = n - 1 - i
-        stars_count = i + 1
-        print(' ' * spaces_count + '* ' * stars_count)
+        print(get_line(i, n))
     for i in range(n - 2, -1, -1):
-        spaces_count = n - 1 - i
-        stars_count = i + 1
-        print(' ' * spaces_count + '* ' * stars_count)
-
+        print(get_line(i, n))
 
 
 print_rhombus(3)
