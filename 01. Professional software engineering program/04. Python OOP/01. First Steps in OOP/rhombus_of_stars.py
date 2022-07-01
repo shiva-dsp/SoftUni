a9f19b9a443b436f1d6fe377d8a34ea93b1ac61a
Emil Enchev <emoen4ev@gmail.com>
2022-07-01 13:36:16 +0300

    Added source code.

--- 01. Professional software engineering program/04. Python OOP/01. First Steps in OOP/rhombus_of_stars.py
+++ 01. Professional software engineering program/04. Python OOP/01. First Steps in OOP/rhombus_of_stars.py
@@ -19,10 +19,36 @@ n = 4
 '''
 
 
+# def get_line(i, n):
+#     spaces_count = n - 1 - i
+#     stars_count = i + 1
+#     return ' ' * spaces_count + ('* ' * stars_count).strip()
+#
+#
+# def print_rhombus(n):
+#     for i in range(n):
+#         print(get_line(i, n))
+#     for i in range(n - 2, -1, -1):
+#         print(get_line(i, n))
+
+
 def get_line(i, n):
     spaces_count = n - 1 - i
     stars_count = i + 1
-    return ' ' * spaces_count + '* ' * stars_count
+    return ' ' * spaces_count + ('* ' * stars_count).strip()
+
+
+def get_rhombus(n):
+    return [get_line(i, n) for i in range(n)] + \
+           [get_line(i, n) for i in range(n - 2, -1, -1)]
+
+
+def print_line(n):
+    print(get_line(n - 1, n - 1))
+
+
+def print_square(n):
+    [print(get_line(n - 1, n - 1)) for _ in range(n)]
 
 
 def print_rhombus(n):
@@ -34,3 +60,4 @@ def print_rhombus(n):
 
 print_rhombus(3)
 print_rhombus(4)
+print_square(5)
