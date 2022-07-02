ff50811fc11443e1b5b3666f3a3139b29444cde5
Emil Enchev <emoen4ev@gmail.com>
2022-07-02 21:49:43 +0300

    Added source code.

--- 01. Professional software engineering program/04. Python OOP/01. First Steps in OOP/class_book.py
+++ 01. Professional software engineering program/04. Python OOP/01. First Steps in OOP/class_book.py
@@ -8,10 +8,25 @@ class Book:
         return f'"{self.name}" by {self.author} has {self.pages}'
 
 
-b = Book('Harry Potter and GoF', 'J.K.Rowling', 500)
+# b = Book('Harry Potter and GoF', 'J.K.Rowling', 500)
+#
+# print(b)
+# print(str(b))
+# print(b.name)
+# print(b.author)
+# print(b.pages)
 
-print(b)
-print(str(b))
-print(b.name)
-print(b.author)
-print(b.pages)
+# class Animal:
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def __str__(self):
+#         return f'{self.name} is the best animal ...'
+#
+#     def sleep(self):
+#         return "sleeping .."
+#
+# animal = Animal("cat")
+# print(animal.sleep())
+# print(animal.name)
+# print(animal)
