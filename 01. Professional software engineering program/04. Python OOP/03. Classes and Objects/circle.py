63e6a1a448b77de667f03bdfc95d906a5f8359ff
Emil Enchev <emoen4ev@gmail.com>
2022-07-04 18:58:34 +0300

    Added source code.

--- 01. Professional software engineering program/04. Python OOP/03. Classes and Objects/circle.py
+++ 01. Professional software engineering program/04. Python OOP/03. Classes and Objects/circle.py
@@ -1,3 +1,20 @@
+# class Circle:
+#     pi = 3.14
+#
+#     def __init__(self, radius: int):
+#         self.radius = radius
+#
+#     def set_radius(self, new_radius: int):
+#         self.radius = new_radius
+#
+#     def get_area(self):
+#         area = Circle.pi * self.radius ** 2
+#         return area
+#
+#     def get_circumference(self):
+#         circumference = 2 * Circle.pi * self.radius
+#         return circumference
+
 class Circle:
     pi = 3.14
 
@@ -8,11 +25,11 @@ class Circle:
         self.radius = new_radius
 
     def get_area(self):
-        area = Circle.pi * self.radius ** 2
+        area = self.pi * self.radius ** 2
         return area
 
     def get_circumference(self):
-        circumference = 2 * Circle.pi * self.radius
+        circumference = 2 * self.pi * self.radius
         return circumference
 
 
