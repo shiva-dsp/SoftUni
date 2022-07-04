09e3f68721b9c798098ef880dc9319022c95beda
Emil Enchev <emoen4ev@gmail.com>
2022-07-04 19:29:39 +0300

    Added source code.

--- 01. Professional software engineering program/04. Python OOP/03. Classes and Objects/glass.py
+++ 01. Professional software engineering program/04. Python OOP/03. Classes and Objects/glass.py
@@ -19,6 +19,28 @@
 #         return f'{space_left} ml left'
 
 
+# class Glass:
+#     initial_content = 0
+#     capacity = 250
+#
+#     def __init__(self):
+#         self.content = self.initial_content
+#
+#     def fill(self, ml):
+#         if ml <= self.capacity - self.content:
+#             self.content += ml
+#             return f'Glass filled with {ml} ml'
+#         return f'Cannot add {ml} ml'
+#
+#     def empty(self):
+#         self.content = 0
+#         return f'Glass is now empty'
+#
+#     def info(self):
+#         space_left = self.capacity - self.content
+#         return f'{space_left} ml left'
+
+
 class Glass:
     initial_content = 0
     capacity = 250
@@ -27,18 +49,23 @@ class Glass:
         self.content = self.initial_content
 
     def fill(self, ml):
-        if ml <= self.capacity - self.content:
-            self.content += ml
-            return f'Glass filled with {ml} ml'
-        return f'Cannot add {ml} ml'
+        space_left = self.calculate_space_left()
+        if space_left < ml:
+            return f'Cannot add {ml} ml'
+
+        self.content += ml
+        return f'Glass filled with {ml} ml'
 
     def empty(self):
         self.content = 0
         return f'Glass is now empty'
 
     def info(self):
-        space_left = self.capacity - self.content
+        space_left = self.calculate_space_left()
         return f'{space_left} ml left'
+
+    def calculate_space_left(self):
+        return self.capacity - self.content
 
 
 glass = Glass()
