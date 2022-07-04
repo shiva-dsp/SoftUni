b25848f2b09b372c327b634bac286c6d90024019
Emil Enchev <emoen4ev@gmail.com>
2022-07-04 19:19:21 +0300

    Added source code.

--- 01. Professional software engineering program/04. Python OOP/03. Classes and Objects/glass.py
+++ 01. Professional software engineering program/04. Python OOP/03. Classes and Objects/glass.py
@@ -1,8 +1,30 @@
+# class Glass:
+#     capacity = 250
+#
+#     def __init__(self):
+#         self.content = 0
+#
+#     def fill(self, ml):
+#         if ml <= self.capacity - self.content:
+#             self.content += ml
+#             return f'Glass filled with {ml} ml'
+#         return f'Cannot add {ml} ml'
+#
+#     def empty(self):
+#         self.content = 0
+#         return f'Glass is now empty'
+#
+#     def info(self):
+#         space_left = self.capacity - self.content
+#         return f'{space_left} ml left'
+
+
 class Glass:
+    initial_content = 0
     capacity = 250
 
     def __init__(self):
-        self.content = 0
+        self.content = self.initial_content
 
     def fill(self, ml):
         if ml <= self.capacity - self.content:
