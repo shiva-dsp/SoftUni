60117bd39bf6767041f174b7ed4fda4cb7539003
Emil Enchev <emoen4ev@gmail.com>
2022-07-05 11:18:52 +0300

    Added source code.

---
+++ 01. Professional software engineering program/04. Python OOP/04. Classes and Objects - Exercise/vet.py
@@ -0,0 +1,44 @@
+class Vet:
+    animals = []
+    space = 5
+
+    def __init__(self, name: str):
+        self.name = name
+        self.animals = []
+
+    def register_animal(self, animal_name: str):
+        if len(Vet.animals) >= Vet.space:
+            return 'Not enough space'
+
+        Vet.animals.append(animal_name)
+        self.animals.append(animal_name)
+        return f'{animal_name} registered in the clinic'
+
+    def unregister_animal(self, animal_name: str):
+        if animal_name not in self.animals:
+            return f'{animal_name} not in the clinic'
+
+        Vet.animals.remove(animal_name)
+        self.animals.remove(animal_name)
+        return f'{animal_name} unregistered successfully'
+
+    def info(self):
+        number_animals_in_current_clinic_ = len(self.animals)
+        space_left_in_clinic = Vet.space - len(Vet.animals)
+        return f'{self.name} has {number_animals_in_current_clinic_} animals. {space_left_in_clinic} space left in ' \
+               f'clinic '
+
+
+peter = Vet("Peter")
+george = Vet("George")
+print(peter.register_animal("Tom"))
+print(george.register_animal("Cory"))
+print(peter.register_animal("Fishy"))
+print(peter.register_animal("Bobby"))
+print(george.register_animal("Kay"))
+print(george.unregister_animal("Cory"))
+print(peter.register_animal("Silky"))
+print(peter.unregister_animal("Molly"))
+print(peter.unregister_animal("Tom"))
+print(peter.info())
+print(george.info())
