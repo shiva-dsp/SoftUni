cc7123ab25fff9f0e6910635f68f01814e371c5f
Emil Enchev <emoen4ev@gmail.com>
2022-07-06 10:04:34 +0300

    Added source code.

---
+++ 01. Professional software engineering program/04. Python OOP/04. Classes and Objects - Exercise/guild_system/project/guild.py
@@ -0,0 +1,13 @@
+class Guild:
+    def __init__(self, name: str):
+        self.name = name
+        self.players = []
+
+    def assign_player(self, player: Player):
+        pass
+
+    def kick_player(self, player_name: str):
+        pass
+
+    def guild_info(self):
+        pass
