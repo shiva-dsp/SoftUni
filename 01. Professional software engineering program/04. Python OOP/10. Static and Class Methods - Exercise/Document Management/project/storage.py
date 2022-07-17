ae5dd9db47fd996cadf66a46e42a6186a54c451f
Emil Enchev <emoen4ev@gmail.com>
2022-07-17 19:34:07 +0300

    Added source code.

--- 01. Professional software engineering program/04. Python OOP/10. Static and Class Methods - Exercise/Document Management/project/storage.py
+++ 01. Professional software engineering program/04. Python OOP/10. Static and Class Methods - Exercise/Document Management/project/storage.py
@@ -9,17 +9,19 @@ class Storage:
         self.topics = []
         self.documents = []
 
+    @staticmethod
+    def __add_item(given_item, given_sequence):
+        if given_item not in given_sequence:
+            given_sequence.append(given_item)
+
     def add_category(self, category: Category):
-        if category not in self.categories:
-            self.categories.append(category)
+        self.__add_item(category, self.categories)
 
     def add_topic(self, topic: Topic):
-        if topic not in self.topics:
-            self.topics.append(topic)
+        self.__add_item(topic, self.topics)
 
     def add_document(self, document: Document):
-        if document not in self.documents:
-            self.documents.append(document)
+        self.__add_item(document, self.documents)
 
     @staticmethod
     def __find_object_by_id(given_id, given_sequence):
@@ -29,16 +31,15 @@ class Storage:
 
     def edit_category(self, category_id: int, new_name: str):
         category = self.__find_object_by_id(category_id, self.categories)
-        category.name = new_name
+        category.edit(new_name)
 
     def edit_topic(self, topic_id: int, new_topic: str, new_storage_folder: str):
         topic = self.__find_object_by_id(topic_id, self.topics)
-        topic.topic = new_topic
-        topic.storage_folder = new_storage_folder
+        topic.edit(new_topic, new_storage_folder)
 
     def edit_document(self, document_id: int, new_file_name: str):
         document = self.__find_object_by_id(document_id, self.documents)
-        document.file_name = new_file_name
+        document.edit(new_file_name)
 
     @staticmethod
     def __delete_item(given_item, given_sequence):
@@ -61,4 +62,5 @@ class Storage:
         return self.__find_object_by_id(document_id, self.documents)
 
     def __repr__(self):
-        return '\n'.join(str(document.__repr__()) for document in self.documents)
+        # return '\n'.join(str(document.__repr__()) for document in self.documents)
+        return '\n'.join([repr(x) for x in self.documents])
