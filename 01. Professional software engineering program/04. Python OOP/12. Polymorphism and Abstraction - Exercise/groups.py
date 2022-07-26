9928bbebe5942d9ab81db71ecb1258ef5d61b1cb
Emil Enchev <emoen4ev@gmail.com>
2022-07-26 20:58:26 +0300

    Added Source code.

---
+++ 01. Professional software engineering program/04. Python OOP/12. Polymorphism and Abstraction - Exercise/groups.py
@@ -0,0 +1,60 @@
+class Person:
+    def __init__(self, name: str, surname: str):
+        self.name = name
+        self.surname = surname
+
+    def __str__(self):
+        return f'{self.name} {self.surname}'
+
+    def __add__(self, other):
+        return Person(self.name, other.surname)
+
+
+class Group:
+    def __init__(self, name: str, people: []):
+        self.name = name
+        self.people = people
+
+    def __len__(self):
+        return len(self.people)
+
+    def __add__(self, other):
+        new_name = f'{self.name} {other.name}'
+        merged_members = self.people + other.people
+        return Group(new_name, merged_members)
+
+    def __str__(self):
+        people_str = ', '.join([str(p) for p in self.people])
+        return f'Group {self.name} with members {people_str}'
+
+    def __getitem__(self, idx):
+        return f'Person {idx}: {str(self.people[idx])}'
+
+
+#  -------------------------------------------- test ----------------
+
+p0 = Person('Aliko', 'Dangote')
+p1 = Person('Bill', 'Gates')
+p2 = Person('Warren', 'Buffet')
+p3 = Person('Elon', 'Musk')
+p4 = p2 + p3
+
+first_group = Group('__VIP__', [p0, p1, p2])
+second_group = Group('Special', [p3, p4])
+third_group = first_group + second_group
+
+print(len(first_group))
+print(second_group)
+print(third_group[0])
+
+for person in third_group:
+    print(person)
+
+# 3
+# Group Special with members Elon Musk, Warren Musk
+# Person 0: Aliko Dangote
+# Person 0: Aliko Dangote
+# Person 1: Bill Gates
+# Person 2: Warren Buffet
+# Person 3: Elon Musk
+# Person 4: Warren Musk
