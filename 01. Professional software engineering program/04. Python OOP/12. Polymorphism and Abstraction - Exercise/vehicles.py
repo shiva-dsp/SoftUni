f83d36f2958c6de54b2e7b81a95ea25b68971602
Emil Enchev <emoen4ev@gmail.com>
2022-07-26 19:38:17 +0300

    Added Source code.

--- 01. Professional software engineering program/04. Python OOP/12. Polymorphism and Abstraction - Exercise/vehicles.py
+++ 01. Professional software engineering program/04. Python OOP/12. Polymorphism and Abstraction - Exercise/vehicles.py
@@ -44,3 +44,22 @@ class Truck(Vehicle):
 
     def refuel(self, fuel: int):
         self.fuel_quantity += self.COEFFICIENT_OF_POSSIBLE_FUEL_QUANTITY * fuel
+
+
+car = Car(20, 5)
+car.drive(3)
+print(car.fuel_quantity)
+car.refuel(10)
+print(car.fuel_quantity)
+
+# 2.299999999999997
+# 12.299999999999997
+
+truck = Truck(100, 15)
+truck.drive(5)
+print(truck.fuel_quantity)
+truck.refuel(50)
+print(truck.fuel_quantity)
+
+# 17.0
+# 64.5
