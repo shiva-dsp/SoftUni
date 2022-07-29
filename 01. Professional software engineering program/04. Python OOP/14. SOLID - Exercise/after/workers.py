9b0d9d489394e10a6ef971cebc428a48433996a9
Emil Enchev <emoen4ev@gmail.com>
2022-07-29 12:27:39 +0300

    Added source code.

--- 01. Professional software engineering program/04. Python OOP/14. SOLID - Exercise/after/workers.py
+++ 01. Professional software engineering program/04. Python OOP/14. SOLID - Exercise/after/workers.py
@@ -25,7 +25,7 @@ class Manager:
         self.worker = None
 
     def set_worker(self, worker):
-        assert isinstance(worker, BaseWorker), '`worker` must be of type {}'.format(BaseWorker)
+        assert isinstance(worker, BaseWorker), f'`worker` must be of type {BaseWorker}'
 
         self.worker = worker
 
