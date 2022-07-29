7262a4cd4b4bc4f9b2171376820db5db94c6376c
Emil Enchev <emoen4ev@gmail.com>
2022-07-29 20:04:31 +0300

    Added sourse code.

--- 01. Professional software engineering program/04. Python OOP/15. Iterators and Generators/reverse_iter.py
+++ 01. Professional software engineering program/04. Python OOP/15. Iterators and Generators/reverse_iter.py
@@ -1,3 +1,5 @@
+# Full implementation
+
 class reverse_iter:
     def __init__(self, values):
         self.values = list(values)
@@ -14,7 +16,29 @@ class reverse_iter:
         self.index -= 1
         return value_to_return
 
+# -----------------------
+
+class reverse_iter2:
+    def __init__(self,values):
+        self.values = reversed(values)
+
+    def __iter__(self):
+        return iter(self.values)
+
 
 reversed_list = reverse_iter([1, 2, 3, 4])
+
 for item in reversed_list:
+    print(item)
+
+for item in reverse_iter([1, 2, 3, 4]):
+    print(item)
+
+for item in reverse_iter({1, 2, 3, 4}):
+    print(item)
+
+for item in reverse_iter2([1, 2, 3, 4]):
+    print(item)
+
+for item in reverse_iter2({1, 2, 3, 4}):
     print(item)
