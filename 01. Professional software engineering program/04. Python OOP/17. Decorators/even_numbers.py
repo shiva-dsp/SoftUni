1d84aa31652266d96fc5e964735a781b54d2317f
Emil Enchev <emoen4ev@gmail.com>
2022-07-30 21:36:07 +0300

    Added solution of even_numbers.py.

---
+++ 01. Professional software engineering program/04. Python OOP/17. Decorators/even_numbers.py
@@ -0,0 +1,24 @@
+from functools import wraps
+
+
+def even_numbers(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return [x for x in result if x % 2 == 0]
+
+    return wrapper
+
+
+# ------------- test --------------
+
+@even_numbers
+def get_numbers(numbers):
+    return numbers
+
+
+print(get_numbers([1, 2, 3, 4, 5]))
+
+# ------------- result -------------------
+
+# [2, 4]
