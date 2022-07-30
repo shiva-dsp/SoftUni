0c39145d4c1d251996f180930584cc8d634e5171
Emil Enchev <emoen4ev@gmail.com>
2022-07-30 16:20:44 +0300

    Added solution of take_halves.py.

---
+++ 01. Professional software engineering program/04. Python OOP/16. Iterators and Generators - Exercise/take_halves.py
@@ -0,0 +1,33 @@
+def solution():
+
+    def integers():
+        num = 1
+        while True:
+            yield num
+            num += 1
+
+    def halves():
+        for i in integers():
+            yield i / 2
+
+    def take(n, seq):
+        return [next(seq) for _ in range(n)]
+
+    return (take, halves, integers)
+
+
+# ------------- tests --------------
+
+take = solution()[0]
+halves = solution()[1]
+print(take(5, halves()))
+
+take = solution()[0]
+halves = solution()[1]
+print(take(0, halves()))
+
+# ------------- results ----------------
+
+# [0.5, 1.0, 1.5, 2.0, 2.5]
+
+# []
