45bc494408221e2fd600abc42bac8ad24c43dc2d
Emil Enchev <emoen4ev@gmail.com>
2022-08-09 21:44:08 +0300

    Added merge_sort.py.

---
+++ 02. Algorithms with Python/03. Searching and Sorting Algorithms/merge_sort.py
@@ -0,0 +1,50 @@
+def merge_arrays(left, right):
+    result = [None] * (len(left) + len(right))
+    left_idx = 0
+    right_idx = 0
+    result_idx = 0
+
+    while left_idx < len(left) and right_idx < len(right):
+        if left[left_idx] < right[right_idx]:
+            result[result_idx] = left[left_idx]
+            left_idx += 1
+        else:
+            result[result_idx] = right[right_idx]
+            right_idx += 1
+        result_idx += 1
+
+    while left_idx < len(left):
+        result[result_idx] = left[left_idx]
+        left_idx += 1
+        result_idx += 1
+
+    while right_idx < len(right):
+        result[result_idx] = right[right_idx]
+        right_idx += 1
+        result_idx += 1
+
+    return result
+
+
+def merge_sort(nums):
+    if len(nums) == 1:
+        return nums
+
+    mid_idx = len(nums) // 2
+    left = nums[:mid_idx]
+    right = nums[mid_idx:]
+
+    return merge_arrays(merge_sort(left), merge_sort(right))
+
+
+nums = [int(x) for x in input().split()]
+
+print(*merge_sort(nums))
+
+# --------- tests ----------
+
+# 5 4 3 2 1
+
+# --------- results ----------
+
+# 1 2 3 4 5
