90f586c63c6e1ca183c5e57aab3f06bc1f83cfe3
Emil Enchev <emoen4ev@gmail.com>
2022-08-10 16:13:29 +0300

    Added cycles_in_graph.py.

---
+++ 02. Algorithms with Python/05. Introduction to Graphs - Exercise/cycles_in_graph.py
@@ -0,0 +1,67 @@
+def dfs(node, graph, visited, cycles):
+    if node in cycles:
+        raise Exception
+    if node in visited:
+        return
+
+    visited.add(node)
+    cycles.add(node)
+
+    for child in graph[node]:
+        dfs(child, graph, visited, cycles)
+
+    cycles.remove(node)
+
+
+graph = {}
+
+while True:
+    line = input()
+    if line == 'End':
+        break
+    source, destination = line.split('-')
+    if source not in graph:
+        graph[source] = []
+    if destination not in graph:
+        graph[destination] = []
+    graph[source].append(destination)
+
+try:
+    visited = set()
+    for node in graph:
+        dfs(node, graph, visited, set())
+    print('Acyclic: Yes')
+except Exception:
+    print('Acyclic: No')
+
+# ------------- tests --------------
+
+# C-G
+# End
+#
+# A-F
+# F-D
+# D-A
+# End
+#
+# E-Q
+# Q-P
+# P-B
+# End
+#
+# K-J
+# J-N
+# N-L
+# N-M
+# M-I
+# End
+
+# --------- results ----------
+
+# Acyclic: Yes
+
+# Acyclic: No
+
+# Acyclic: Yes
+
+# Acyclic: Yes
