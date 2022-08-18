528455586f8b5378f497d8fb381d1ffdb8d79449
Emil Enchev <emoen4ev@gmail.com>
2022-08-18 13:57:58 +0300

    Added information about testing the solution.

--- 01. Professional software engineering program/03. Python Advanced/21. Retake Exam - 18_08_2022/stewards_problem_description.py
+++ 01. Professional software engineering program/03. Python Advanced/21. Retake Exam - 18_08_2022/stewards_problem_description.py
@@ -47,3 +47,28 @@ while sequence_of_seats:
 
 print(f"Seat matches: {', '.join(seat_matches)}")
 print(f"Rotations count: {rotations_count}")
+
+# ------------- tests --------------
+
+# 17K, 20B, 3C, 15D, 31Z, 28F
+# 20, 35, 15, 3, 2, 10
+# 1, 15, 64, 53, 45, 46
+
+# 25A, 16B, 44T, 49D, 27M, 44F
+# 25, 3, 31, 49, 26, 13
+# 10, 15, 44, 40
+
+# 15C, 25C, 36C, 43P, 40E, 38G
+# 15, 25, 80, 40, 15, 99, 52
+# 15, 42, 29
+
+# --------- results ----------
+
+# Seat matches: 20B, 15D, 3C
+# Rotations count: 4
+
+# Seat matches: 25A, 44F
+# Rotations count: 10
+
+# Seat matches: 25C, 40E, 15C
+# Rotations count: 7
