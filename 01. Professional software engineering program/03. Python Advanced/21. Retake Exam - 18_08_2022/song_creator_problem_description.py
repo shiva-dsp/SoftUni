1c0a4d6dad6030bc6ab7204bebeef53ccff647dc
Emil Enchev <emoen4ev@gmail.com>
2022-08-18 14:05:11 +0300

    Added information about testing the solution.

--- 01. Professional software engineering program/03. Python Advanced/21. Retake Exam - 18_08_2022/song_creator_problem_description.py
+++ 01. Professional software engineering program/03. Python Advanced/21. Retake Exam - 18_08_2022/song_creator_problem_description.py
@@ -23,12 +23,70 @@ def add_songs(*args):
 
     return total_result
 
+# --------- tests ----------
 
-print(add_songs(
-    ("Bohemian Rhapsody", []),
-    ("Just in Time",
-     ["Just in time, I found you just in time",
-      "Before you came, my time was running low",
-      "I was lost, the losing dice were tossed",
-      "My bridges all were crossed, nowhere to go"])
-))
+# print(add_songs(
+#     ("Bohemian Rhapsody", []),
+#     ("Just in Time",
+#      ["Just in time, I found you just in time",
+#       "Before you came, my time was running low",
+#       "I was lost, the losing dice were tossed",
+#       "My bridges all were crossed, nowhere to go"])
+# ))
+
+
+# print(add_songs(
+#     ("Beat It", []),
+#     ("Beat It",
+#      ["Just beat it (beat it), beat it (beat it)",
+#       "No one wants to be defeated"]),
+#     ("Beat It", []),
+#     ("Beat It",
+#      ["Showin' how funky and strong is your fight",
+#       "It doesn't matter who's wrong or right"]),
+# ))
+
+
+# print(add_songs(
+#     ("Love of my life",
+#      ["Love of my life, you've hurt me",
+#       "You've broken my heart, and now you leave me",
+#       "Love of my life, can't you see?",
+#       "Bring it back, bring it back"]),
+#     ("Beat It", []),
+#     ("Love of my life",
+#      ["Don't take it away from me",
+#       "Because you don't know",
+#       "What it means to me"]),
+#     ("Dream On",
+#      ["Every time that I look in the mirror"]),
+# ))
+
+# --------- results ----------
+
+# - Bohemian Rhapsody
+# - Just in Time
+# Just in time, I found you just in time
+# Before you came, my time was running low
+# I was lost, the losing dice were tossed
+# My bridges all were crossed, nowhere to go
+
+
+# - Beat It
+# Just beat it (beat it), beat it (beat it)
+# No one wants to be defeated
+# Showin' how funky and strong is your fight
+# It doesn't matter who's wrong or right
+
+
+# - Love of my life
+# Love of my life, you've hurt me
+# You've broken my heart, and now you leave me
+# Love of my life, can't you see?
+# Bring it back, bring it back
+# Don't take it away from me
+# Because you don't know
+# What it means to me
+# - Beat It
+# - Dream On
+# Every time that I look in the mirror
