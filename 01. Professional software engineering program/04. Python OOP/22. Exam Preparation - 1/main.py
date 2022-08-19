e66ff0951f9479f77b37e3c7dae16caa753332d5
Emil Enchev <emoen4ev@gmail.com>
2022-08-19 21:30:45 +0300

    Added source code.

---
+++ 01. Professional software engineering program/04. Python OOP/22. Exam Preparation - 1/main.py
@@ -0,0 +1,14 @@
+# from project.supply.drink import Drink
+# from project.supply.food import Food
+#
+# food_25 = Food('banana')
+# print(food_25.energy)
+#
+# food = Food('banana', 2)
+# print(food.energy)
+#
+# drink_15 = Drink('wine')
+# print(drink_15.energy)
+#
+# drink = Drink('wine', 3)  # No second argument is expected. Filed as default.
+# print(drink.energy)
