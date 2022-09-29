4b2e8dc25d438987896848c1fd31ecca9c7e1878
Emil Enchev <emoen4ev@gmail.com>
2022-09-29 20:16:43 +0300

    Performed migrations to DB tasks_db.

--- 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/03. Django Introduction/django101/django101/tasks/models.py
+++ 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/03. Django Introduction/django101/django101/tasks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+# Maps to a DB Table
+class Task(models.Model):
+    # varchar(30) NOT NULL
+    name = models.CharField(
+        max_length=30,
+        null=False,
+    )
+
+    description = models.TextField()
+
+    priority = models.IntegerField()
