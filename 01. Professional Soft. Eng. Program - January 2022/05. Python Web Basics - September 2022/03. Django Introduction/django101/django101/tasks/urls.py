111d9a27b14fec1e37416174591bbb1f4ee1c6db
Emil Enchev <emoen4ev@gmail.com>
2022-10-03 12:15:35 +0300

    Added 'it_works' as path in django101/tasks/urls.py.

--- 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/03. Django Introduction/django101/django101/tasks/urls.py
+++ 01. Professional Soft. Eng. Program - January 2022/05. Python Web Basics - September 2022/03. Django Introduction/django101/django101/tasks/urls.py
@@ -5,8 +5,8 @@ from django.urls import path
 from django101.tasks.views import show_bare_minimum_view, show_all_tasks
 
 urlpatterns = (
-    # localhost:8000/tasks/ or http://localhost:8000/tasks
-    path("", show_bare_minimum_view),
+    # localhost:8000/tasks/ or http://localhost:8000/tasks/it_works
+    path("it_works/", show_bare_minimum_view),
     # localhost:8000/tasks/all or http://localhost:8000/tasks/all
     path("all/", show_all_tasks),
     # localhost:8000/tasks/create/
